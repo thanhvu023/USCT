e302feefab1732ff4e05ac4e74e817a075b59486
ThanhVu <huynhthanhvu115@gmail.com>
2024-03-13 20:17:30 +0700

    delete slider

--- src/components/section-components/Slider.js
+++ src/components/section-components/Slider.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import Slider from 'react-slick';
+// import React from 'react';
+// import Slider from 'react-slick';
 
-export default function SliderComponent() {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
+// export default function SliderComponent() {
+//     const settings = {
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//     };
 
-    return (
-        <Slider {...settings}>
-            <div>
-                <img src="https://www.studylink.org/vnt_upload/weblink/Website_slide_banner_5.png" alt="Du học Mỹ 1"
-                // style={{maxHeight:'500px'}}
-                />
-            </div>
-            <div>
-            <img src="https://www.hsbc.com.vn/content/dam/hsbc/hbvn/images/wealth/hero-21-9/students-walking-friends-books-pwsimg-2658.jpg" alt="Du học Mỹ 1"/>
-            </div>
-            <div>
-            <img src="https://eduurban.com/wp-content/uploads/2022/09/Web_Banner_Edu_Urban_Study_abroad_-_Slide_2_Study_Abroad_-Preparation.jpg" alt="Du học Mỹ 1"/>
-            </div>
-            {/* Add more images here */}
-        </Slider>
-    );
-}
+//     return (
+//         <Slider {...settings}>
+//             <div>
+//                 <img src="https://www.studylink.org/vnt_upload/weblink/Website_slide_banner_5.png" alt="Du học Mỹ 1"
+//                 // style={{maxHeight:'500px'}}
+//                 />
+//             </div>
+//             <div>
+//             <img src="https://www.hsbc.com.vn/content/dam/hsbc/hbvn/images/wealth/hero-21-9/students-walking-friends-books-pwsimg-2658.jpg" alt="Du học Mỹ 1"/>
+//             </div>
+//             <div>
+//             <img src="https://eduurban.com/wp-content/uploads/2022/09/Web_Banner_Edu_Urban_Study_abroad_-_Slide_2_Study_Abroad_-Preparation.jpg" alt="Du học Mỹ 1"/>
+//             </div>
+//             {/* Add more images here */}
+//         </Slider>
+//     );
+// }
