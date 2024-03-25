ceb1570fdb01fe0984c24860ddbdf774022c4b03
nhutnam <namhuynh2610@gmail.com>
2024-03-25 21:26:37 +0700

    fix nav

--- src/components/global-components/navbar.js
+++ src/components/global-components/navbar.js
@@ -124,19 +124,19 @@ function Navbar() {
             {token && (
                         <div className="nav-right-part nav-right-part-desktop d-flex align-items-center">
 
-                <Dropdown as="li" className="nav-item header-profile">              
+                <Dropdown className="nav-item header-profile">              
                   <Dropdown.Toggle to={"#"} className="nav-link i-false" as="div">
                   <img src={publicUrl + "assets/img/author/pic2.jpg"} alt="img" 
-                            className="bg-info rounded-circle mb-4 "
-                            style={{width:"50px",height:'50px'}}
+                            className="bg-info rounded-circle "
+                            style={{width:"50px"}}
                   />
                   </Dropdown.Toggle>
                   <Dropdown.Menu align="end" className="mt-3 dropdown-menu dropdown-menu-right ">
                       <Link to={"/customer-profile"} className="dropdown-item ai-icon icon-bell-effect ml-0">
-                          <span className="ms-2">Customer's profile </span>
+                          <span className="ms-2">Customer&apos;s profile </span>
                       </Link>
                       <Link to={"/student-profile"} className="dropdown-item ai-icon ml-0">
-                          <span className="ms-2">Student's profile </span>
+                          <span className="ms-2">Student&apos;s profile </span>
                       </Link>                     
                       <button  className="dropdown-item ai-icon" onClick={handleLogout}>
                 <svg
