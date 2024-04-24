1d95a4c282e691889bd3b43e65bb32d576c28fa0
ThanhVu <huynhthanhvu115@gmail.com>
2024-04-24 21:36:13 +0700

    staff-page

---
+++ src/components/admin-components/side-bar/side-bar-staff.jsx
@@ -0,0 +1,182 @@
+import React, { useReducer, useEffect, useState } from "react";
+import { Collapse } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { MenuList } from "./menu-staff";
+import './sidebar.css';
+
+const reducer = (previousState, updatedState) => ({
+  ...previousState,
+  ...updatedState,
+});
+
+const initialState = {
+  active: "",
+  activeSubmenu: "",
+};
+
+const SideBarStaff = ({ setMain }) => {
+  const [state, setState] = useReducer(reducer, initialState);
+  const [selectedFilter, setSelectedFilter] = useState(null);
+
+
+
+ 
+  
+  const handleFilterClick = (filterName) => {
+    setSelectedFilter(filterName);
+    setMain(filterName);
+  };
+
+  const handleDashboardClick = () => {
+    setSelectedFilter("admin");
+    setMain("admin");
+  };
+
+  let path = window.location.pathname;
+  path = path.split("/");
+  path = path[path.length - 1];
+
+  useEffect(() => {
+    MenuList.forEach((data) => {
+      data.content?.forEach((item) => {
+        if (path === item.to) {
+          setState({ active: data.title });
+        }
+        item.content?.forEach((ele) => {
+          if (path === ele.to) {
+            setState({ activeSubmenu: item.title, active: data.title });
+          }
+        });
+      });
+    });
+  }, [path]);
+
+  return (
+    <div>
+      <div className="dlabnav-scroll">
+        <ul className="list-group list-group-flush" id="menu">
+          {MenuList.map((data, index) => {
+            let menuClass = data.classsChange;
+            if (menuClass === "menu-title") {
+              return (
+                <li
+                  className={`list-group-item ${menuClass} ${
+                    data.extraclass ? data.extraclass : ""
+                  }`}
+                  key={index}
+                >
+                  {data.title}
+                </li>
+              );
+            } else {
+              return (
+                <li
+                  className={`list-group-item ${
+                    state.active === data.title ? "active" : ""
+                  }`}
+                  key={index}
+                >
+                  <Link
+                    to="#"
+                    className={`nav-link has-arrow ${
+                      selectedFilter === data.title ? "selected" : ""
+                    }`}
+                    onClick={
+                      data.filterName === "Thống kê"
+                        ? handleDashboardClick
+                        : () => handleFilterClick(data.title)
+                    }
+                    onMouseEnter={() => {
+                      if (state.active !== data.title) {
+                        setState({ active: data.title });
+                      }
+                    }}
+                  
+                  >
+                    {data.iconStyle}
+                    <span className="nav-text">{data.title}</span>
+                    <span className="badge badge-xs style-1 badge-danger">
+                      {data.update}
+                    </span>
+                  </Link>
+                  <Collapse in={state.active === data.title}>
+                    <ul
+                      className={`${
+                        menuClass === "mm-collapse" ? "mm-show" : ""
+                      }`}
+                    >
+                      {data.content &&
+                        data.content.map((data, index) => (
+                          <li
+                            key={index}
+                            className={`${
+                              state.activeSubmenu === data.title ? "active" : ""
+                            }`}
+                          >
+                            <Link
+                              to={data.to}
+                              className={`nav-link ${
+                                data.hasMenu ? "has-arrow" : ""
+                              } ${
+                                selectedFilter === data.title ? "selected" : ""
+                              }`}
+                              onMouseEnter={() => {
+                                if (state.activeSubmenu !== data.title) {
+                                  setState({ activeSubmenu: data.title });
+                                }
+                              }}
+                              onMouseLeave={() => {
+                                if (state.activeSubmenu === data.title) {
+                                  setState({ activeSubmenu: "" });
+                                }
+                              }}
+                            >
+                              {data.title}
+                            </Link>
+                            <Collapse in={state.activeSubmenu === data.title}>
+                              <ul
+                                className={`${
+                                  menuClass === "mm-collapse" ? "mm-show" : ""
+                                }`}
+                              >
+                                {data.content &&
+                                  data.content.map((data, index) => (
+                                    <li key={index}>
+                                      <Link
+                                        className={`nav-link ${
+                                          path === data.to ? "active" : ""
+                                        }`}
+                                        to={data.to}
+                                      >
+                                        {data.title}
+                                      </Link>
+                                    </li>
+                                  ))}
+                              </ul>
+                            </Collapse>
+                          </li>
+                        ))}
+                    </ul>
+                  </Collapse>
+                </li>
+              );
+            }
+          })}
+        </ul>
+        <div className="copyright">
+          {/* <p>Edumin  Admin © {new Date().getFullYear()}</p> */}
+          <p className="fs-12">
+            Made with{" Thesis"}
+            <span
+              className="heart"
+              onClick={(e) => e.target.classList.toggle("heart-blast")}
+            ></span>{" "}
+            by USCT
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SideBarStaff;
---
+++ src/components/staff-page.jsx
@@ -0,0 +1,56 @@
+import React, { Fragment, useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate từ react-router-dom
+import AdminHome from "./admin-components/home";
+import SideBarAd from "./admin-components/side-bar/SideBar";
+import NavHader from "./admin-components/NavHader";
+import Footer from "./global-components/footer";
+import AllCustomer from "./admin-components/customer-components/all-customer";
+import Test1 from "./admin-components/customer-components/test1";
+import AllConsultant from "./admin-components/consultant-components/consutant";
+import AllProgramsPage from "./admin-components/program-components/programs1";
+import AllPrograms from "./admin-components/program-components/programs";
+import Registration from "./admin-components/registration-components/registration";
+import ProgramApplicationPage from "./admin-components/program-application-components/program-apllicationbyStudentprofileId";
+
+
+const StaffPage = () => {
+  const [main, setMain] = useState("admin");
+
+  const handleAllConsultantClick = () => {
+    setMain("Tư vấn viên");
+  };
+
+  const componentMap = {
+
+    "Chương trình": <AllPrograms />,
+    "Hồ sơ đăng ký": <ProgramApplicationPage />,
+    "Đơn tư vấn": <Registration />,
+    "Test1": <Test1 />,
+  };
+
+  const getContentComponent = (main) => {
+    const selectedComponent = componentMap[main];
+    return selectedComponent || <AdminHome handleAllConsultantClick={handleAllConsultantClick} />;
+  };
+
+  const renderContent = () => (
+    <div className="content-wrapper" style={{ flex: '10', display: 'flex', flexDirection: 'column' }}> 
+      {getContentComponent(main)}
+    </div>
+  );
+
+  return (
+    <div id="main-wrapper" >  
+      <div id="content-wrapper" className="d-flex flex-row"> 
+        <div className="sidebar-wrapper" style={{ flex: '2' }}> 
+          <NavHader/>
+          <SideBarAd setMain={setMain} />
+        </div>
+        {renderContent()}
+      </div>
+    </div>
+  );
+};
+
+export default StaffPage;
+
