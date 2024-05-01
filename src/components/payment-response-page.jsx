bb9bb2cf6dd39df8af0f33b8796c49e83a3a926c
ThanhVu <huynhthanhvu115@gmail.com>
2024-05-01 19:17:35 +0700

    add animations payment

--- src/components/payment-response-page.jsx
+++ src/components/payment-response-page.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import handleVnPayResponse from '../redux/slice/handleVnPayResponse';
-import { CircularProgress, Typography, Button, Box } from '@mui/material';
-
+import { CircularProgress, Typography, Button, Box, Card, CardContent } from '@mui/material';
+import Lottie from 'react-lottie';
+import successAnimation from '../components/admin-components/customer-components/contants/animations/paymentsuccess.json';  // Đảm bảo rằng bạn đã import file json animation
 
 const PaymentResponsePage = () => {
   const location = useLocation();
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
   const [success, setSuccess] = useState(false);
@@ -26,13 +28,21 @@ const PaymentResponsePage = () => {
     }
   }, [location.search]);
 
+  const defaultOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: successAnimation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
   if (loading) {
     return (
       <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="100vh">
         <CircularProgress />
         <Typography variant="h6" marginTop={2}>
-          Processing your payment, please wait...
-        </Typography>
+        Đang xử lý khoản thanh toán của bạn, vui lòng đợi...        </Typography>
       </Box>
     );
   }
@@ -51,14 +61,20 @@ const PaymentResponsePage = () => {
   }
 
   return (
-    <Box display="flex" flexDirection="column" alignItems="center" justifyContent="center" minHeight="100vh">
-      <Typography variant="h6" color="primary">
-        Payment processed successfully!
-      </Typography>
-      <Button variant="contained" color="primary" onClick={() => window.location.href = '/'}>
-        Return Home
-      </Button>
-    </Box>
+    <Card sx={{ maxWidth: 345, m: "auto", mt: 5 }}>
+      <CardContent>
+        {success && (
+          <Lottie options={defaultOptions} height={200} width={200} />
+        )}
+        <Typography variant="h5" gutterBottom component="div" color="primary" textAlign="center">
+        Thanh toán được xử lý thành công!       
+         </Typography>
+        <Button variant="contained" color="primary" onClick={() => navigate(`/program-application-detail/${location.search.get('programApplicationId')}`)}>
+      Trở về trang chi tiết chương trình đã ứng tuyển !
+
+        </Button>
+      </CardContent>
+    </Card>
   );
 };
 
